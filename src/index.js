2b0ae265260175df2e0aed68db0e0a95bb9b3214
petroDavydov <petrodavydov@gmail.com>
2021-08-24 17:42:46 +0300

    pretteir work

--- src/index.js
+++ src/index.js
@@ -18,22 +18,18 @@ refs.controls.addEventListener("click", (event) => {
 
   if (currentActiveContolItem) {
     currentActiveContolItem.classList.remove("controls__item--active");
-
-	const paneId =currentActiveContolItem.getAttribute('href').slice(1)
-	const pane =refs.panes.querySelector(`#${paneId}`)
-	pane.classList.remove('pane--active')
+    const paneId = currentActiveContolItem.getAttribute("href").slice(1);
+    const pane = refs.panes.querySelector(`#${paneId}`);
+    pane.classList.remove("pane--active");
   }
 
   const controlItem = event.target;
   controlItem.classList.add("controls__item--active");
 
   const paneId = controlItem.getAttribute("href").slice(1);
-
-  console.log(paneId);
+  //   console.log(paneId);
 
   const pane = refs.panes.querySelector(`#${paneId}`);
-  console.log(pane);
-
-
-  pane.classList.add('pane--active')
+  //   console.log(pane);
+  pane.classList.add("pane--active");
 });
